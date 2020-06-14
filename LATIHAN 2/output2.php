cd9760bfc891e332b64efe807fa27e0fcaa9f4db
fairuz635 <62319813+fairuz635@users.noreply.github.com>
2020-06-14 22:52:06 +0700

    Add files via upload

---
+++ LATIHAN 2/output2.php
@@ -0,0 +1,46 @@
+<html>
+<body>
+<b><center>MUHAMMAD FAIRUZ BAHRAIN | 1800016138</center></b>
+<BR>
+<b><center>TEKWEB LANJUT C</center></b>
+<BR>
+<b><center>PRAKTIKUM TEKWEB C</center></b>
+<BR>
+<BR>
+<table border="1" align="center"
+<tr>
+	<td width="117">NIM</td>
+	<td width="14">:</td>
+	<td width="218"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+	<td>NAMA</td>
+	<td>:</td>
+	<td><?php echo $_POST["NAMA"] ?></td>
+</tr>
+<tr>
+	<td>JENIS KELAMIN</td>
+	<td>:</td>
+	<td><?php echo $_POST["KELAMIN"] ?></td>
+</tr>
+<tr>
+	<td>ALAMAT</td>
+	<td>:</td>
+	<td><?php echo $_POST["ALAMAT"] ?></td>
+</tr>
+<tr>
+	<td>PROGRAM STUDY</td>
+	<td>:</td>
+	<td><?php echo $_POST["PRODI"] ?></td>
+</tr>
+<tr>
+	<td>BAHASA PEMOGRAMAN YANG DIKUASAI</td>
+	<td>:</td>
+	<td><?php echo $_POST["PEMOGRAMAN"] ?></td>
+</tr>
+<tr>
+	<td align="center" colspan="3"><a href="latihan2.html">BACK </a></td>
+</tr>
+</table>
+</body>
+</html>
